83df443792c37b76a3624b654fff1916cef273d7
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-11-21 14:20:34 +0300

    test

---
+++ src/modules/part/part.tsx
@@ -0,0 +1,49 @@
+'use client'
+import { FC } from 'react'
+import classNames from 'classnames'
+
+import styles from './part.module.scss'
+import { PartProps } from './part.types'
+
+import { Canvas } from '@react-three/fiber'
+import { Environment } from '@react-three/drei'
+import { Model } from '@/components/astronautAnimation/astronautAnimation'
+
+const Part: FC<PartProps> = ({
+  className
+}) => {
+  const rootClassName = classNames(styles.root, className)
+
+  return (
+    <div className={rootClassName} id='mascot' style={{ height: '200vh', position: 'relative' }}>
+      <Canvas
+        style={{ width: '100%', height: '100vh', backgroundColor: 'black', position: 'fixed' }}
+        camera={{ position: [0, 0, 0.5], far: 50, fov: 50 }}
+      >
+        <axesHelper />
+        {/* Добавляем окружение для реалистичных отражений */}
+        <Environment preset="sunset" background />
+
+        {/* Базовое освещение */}
+        <ambientLight intensity={0.5} />
+        <directionalLight
+          position={[10, 10, 5]}
+          intensity={1}
+          castShadow
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <pointLight position={[-10, -10, -10]} intensity={0.5} />
+
+        <Model position={[0.2, -0.2, 0]} />
+      </Canvas>
+    </div>
+  )
+}
+
+export default Part
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -3,12 +3,14 @@ import classNames from 'classnames'
 
 import styles from './home.module.scss'
 import { HomeProps } from './home.types'
+import { Part } from '@/modules/part'
 
 const Home: FC<HomeProps> = ({ className }) => {
   const rootClassName = classNames(styles.root, className)
 
   return (
     <main className={rootClassName}>
+      <Part />
     </main>
   )
 }
