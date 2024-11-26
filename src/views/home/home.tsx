f2bce88d0bb673950eca26381e44f7176330691e
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-11-26 16:55:15 +0300

    fix error

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -14,6 +14,7 @@ import { Mission } from '@/modules/mission'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
+import { Part } from '@/modules/part'
 
 gsap.registerPlugin(ScrollTrigger, useGSAP)
 
