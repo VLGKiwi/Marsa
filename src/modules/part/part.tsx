64aa08f091188e43764683f584098e33d8ab3e03
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-12-05 16:50:08 +0300

    stop

--- src/modules/part/part.tsx
+++ src/modules/part/part.tsx
@@ -1,29 +1,72 @@
+// src/components/Part.tsx
 'use client'
-import { FC } from 'react'
+
+import React, { FC, useRef, useEffect } from 'react'
 import classNames from 'classnames'
 
 import styles from './part.module.scss'
 import { PartProps } from './part.types'
 
 import { Canvas } from '@react-three/fiber'
-import { Environment } from '@react-three/drei'
 import Model from '@/components/astronautAnimation/astronautAnimation'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { Environment } from '@react-three/drei'
 
-const Part: FC<PartProps> = ({
-  className
-}) => {
+gsap.registerPlugin(ScrollTrigger)
+
+const Part: FC<PartProps> = ({ className }) => {
   const rootClassName = classNames(styles.root, className)
+  const mascotRef = useRef<HTMLDivElement>(null)
+  const scrollProgressRef = useRef<number>(0)
+
+  // Определите коэффициент скорости
+  const speedFactor = 0.1 // Меняйте значение для регулировки скорости (0.5 = вдвое медленнее)
+
+  useEffect(() => {
+    const mascot = mascotRef.current
+    if (!mascot) {
+      console.warn('mascotRef is null')
+      return
+    }
+
+    // Рассчитайте конец прокрутки на основе speedFactor
+    const scrollEndPercentage = 100 / speedFactor // Например, 200% для speedFactor=0.5
+
+    // Настройка ScrollTrigger
+    const scrollTrigger = ScrollTrigger.create({
+      trigger: mascot,
+      start: 'top top',
+      end: `+=${scrollEndPercentage}%`, // Увеличиваем область прокрутки
+      scrub: true,
+      pin: true,
+      markers: true, // Установите true для отладки, false для продакшена
+      onUpdate: (self) => {
+        scrollProgressRef.current = self.progress
+        console.log('Scroll progress:', self.progress) // Для отладки
+      },
+    })
+
+    return () => {
+      scrollTrigger.kill()
+    }
+  }, [speedFactor])
 
   return (
-    <div className={rootClassName} id='mascot' style={{ height: '200vh', position: 'relative' }}>
+    <div
+      className={rootClassName}
+      id='mascot'
+      ref={mascotRef}
+      style={{ height: `${100 / speedFactor}vh`, position: 'relative' }} // Установите высоту пропорционально speedFactor
+    >
       <Canvas
-        style={{ width: '100%', height: '100vh', backgroundColor: 'black', position: 'fixed' }}
+        style={{
+          width: '100%',
+          height: '100vh',
+          backgroundColor: 'black',
+        }}
         camera={{ position: [0, 0, 0.5], far: 50, fov: 50 }}
       >
-        <axesHelper />
-        {/* Добавляем окружение для реалистичных отражений */}
-        <Environment preset="sunset" background />
-
         <ambientLight intensity={0.5} />
         <directionalLight
           position={[10, 10, 5]}
@@ -37,9 +80,11 @@ const Part: FC<PartProps> = ({
           shadow-camera-top={10}
           shadow-camera-bottom={-10}
         />
+        {/* <Environment files={'./outer-space.exr'}/> */}
         <pointLight position={[-10, -10, -10]} intensity={0.5} />
 
-        <Model position={[0.2, -0.2, 0]} />
+        {/* Передача scrollProgressRef и position в Model */}
+        <Model scrollProgressRef={scrollProgressRef} position={[0.2, -0.2, 0]} />
       </Canvas>
     </div>
   )
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -4,12 +4,12 @@ import classNames from 'classnames'
 import styles from './home.module.scss'
 import { HomeProps } from './home.types'
 import { Part } from '@/modules/part'
-import { Button } from '@/ui'
-import { Traffic } from '@/modules/traffic'
+// import { Button } from '@/ui'
+// import { Traffic } from '@/modules/traffic'
 import { Introduce } from '@/modules/introduce'
 import { Faq } from '@/modules/faq'
-import { Gumbit } from '@/modules/gumbit'
-import Link from 'next/link'
+// import { Gumbit } from '@/modules/gumbit'
+// import Link from 'next/link'
 
 const Home: FC<HomeProps> = ({ className }) => {
   const rootClassName = classNames(styles.root, className)
@@ -17,11 +17,11 @@ const Home: FC<HomeProps> = ({ className }) => {
   return (
     <main className={rootClassName}>
       <Introduce />
-      <Link href='/vacancy'>
+      {/* <Link href='/vacancy'>
         <Button size='sm'>ВАКАНСИИ</Button>
       </Link>
-      <Traffic />      
-      <Gumbit />
+      <Traffic />
+      <Gumbit /> */}
       <Part />
       <Faq />
     </main>
