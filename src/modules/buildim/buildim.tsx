5db850850243e8baafc667f37201001dfc45798e
VLGKiwi <temav.i.p@mail.ru>
2024-12-13 17:20:24 +0300

    fix error

--- src/modules/buildim/buildim.tsx
+++ src/modules/buildim/buildim.tsx
@@ -6,15 +6,13 @@ import classNames from 'classnames';
 import styles from './buildim.module.scss';
 import { BuildimProps } from './buildim.types';
 import { BuildimAstronaut, BuildimCards } from '@/components';
-import { Modal, TitleGradient } from '@/ui';
+import { TitleGradient } from '@/ui';
 
 const Buildim: FC<BuildimProps> = ({ className }) => {
   const rootClassName = classNames(styles.root, className);
 
   // Состояние для текущей активной картинки
   const [activeImage, setActiveImage] = useState<string | null>(null);
-  const [isModalOpen, setModalOpen] = useState(false);
-
 
   return (
     <div className={rootClassName}>
@@ -22,13 +20,7 @@ const Buildim: FC<BuildimProps> = ({ className }) => {
       <h1 className={styles.title}>
         <TitleGradient className="title__large title__vacancy" text="Собрали лучших и продолжаем искать" />
       </h1>
-
-      <button onClick={() => setModalOpen(true)}>Открыть модальное окно</button>
-      <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
-        <h2><TitleGradient text={'Media buyer'} /></h2>
-        <p>Здесь может быть любой контент.</p>
-      </Modal>
-      <BuildimCards onHoverCard={setActiveImage} onClick={() => setModalOpen(true)} />
+      <BuildimCards onHoverCard={setActiveImage}/>
 
       {/* Передаем активную картинку в BuildimAstronaut */}
       <BuildimAstronaut activeImage={activeImage} />
